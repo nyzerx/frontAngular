91472af8a45c4b7b741af6da933477bcbdcce52f
Vicente Gayoso <vicente.gayoso1901@alumnos.ubiobio.cl>
2023-07-19 12:45:48 -0400

    Update formulario-flotante.component.ts

--- src/app/formulario-flotante/formulario-flotante.component.ts
+++ src/app/formulario-flotante/formulario-flotante.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import Swal from 'sweetalert2';
-import { ISolicitud } from '../Clases/Publi';
 import { FormularioSolicitudService } from '../formulario-flotante.service';
 
 @Component({
@@ -16,7 +15,6 @@ export class FormularioFlotanteComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<FormularioFlotanteComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any,
     private formularioSolicitudService: FormularioSolicitudService
   ) {}
 
@@ -38,18 +36,27 @@ export class FormularioFlotanteComponent implements OnInit {
       // Mostrar SweetAlert de éxito
       Swal.fire({
         icon: 'success',
-        title: 'Exito',
+        title: 'Éxito',
         text: 'Su objeto lo puede recuperar en Lab Centrales Fdo. May.'
       }).then(() => {
         // Cierra el formulario flotante sin restablecerlo
         this.dialogRef.close();
       });
 
-      this.formularioSolicitudService.crearSolicitud(this.formulario.value).subscribe(
-        respuesta => {
+      // Crear objeto con datos del formulario
+      const formularioSolicitud = {
+        nombre: this.formulario.value.nombre,
+        apellido: this.formulario.value.apellido,
+        correo: this.formulario.value.correo,
+        motivo: this.formulario.value.motivo
+      };
+
+      // Llamar al servicio para enviar el formulario
+      this.formularioSolicitudService.crearSolicitud(formularioSolicitud).subscribe(
+        (respuesta) => {
           console.log('Formulario de solicitud guardado correctamente:', respuesta);
         },
-        error => {
+        (error) => {
           console.error('Error al guardar el formulario de solicitud:', error);
         }
       );
